a0014fdf99d45c9198979644ca8fe31d91d8329b
amanpoddar-dev12 <amanpoddar775@gmail.com>
2024-09-21 01:30:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require("express");
+import express, { json } from "express";
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json()); // Middleware to parse JSON requests
+app.use(json()); // Middleware to parse JSON requests
 
 // In-memory storage (temporary)
 let dataStore = [
